4c675d5754ab560a1099e38dde9d301f1cb0b49c
henriqueandrade01 <37003470+henriqueandrade01@users.noreply.github.com>
2021-05-20 22:33:28 -0300

    Inclusão do formulário em HTML bem como arquivos CSS e JS

---
+++ logica.js
@@ -0,0 +1,119 @@
+//Validação de CPF
+function is_cpf(c) {
+
+    if ((c = c.replace(/[^\d]/g, "")).length != 11)
+        return false
+    if (c == '00000000000') {
+        return false;
+    }
+    var r;
+    var s = 0;
+    for (i = 1; i <= 9; i++)
+        s = s + parseInt(c[i - 1]) * (11 - i);
+    r = (s * 10) % 11;
+    if ((r == 10) || (r == 11))
+        r = 0;
+    if (r != parseInt(c[9]))
+        return false;
+    s = 0;
+    for (i = 1; i <= 10; i++)
+        s = s + parseInt(c[i - 1]) * (12 - i);
+    r = (s * 10) % 11;
+    if ((r == 10) || (r == 11))
+        r = 0;
+    if (r != parseInt(c[10]))
+        return false;
+    return true;
+}
+//Macara do cpf
+function Mascara(objeto, mascara) {
+    obj = objeto
+    masc = mascara
+    setTimeout("fMascaraEx()", 1)
+}
+function fMascaraEx() {
+    obj.value = masc(obj.value)
+}
+function vCpf(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
+}
+checarCpf = function (el) {
+    document.getElementById('cpfResponse').innerHTML = is_cpf(el.value) ? '<span style="color:green">CPF Válido</span>' : '<span style="color:red">CPF Válido</span>';
+    if (el.value == '') document.getElementById('cpfResponse').innerHTML = '';
+}
+
+//webservice
+//Função CEP
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep
+    document.getElementById('endreco').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores
+    document.getElementById('endereco').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+} 
+else {
+    //CEP não Encontrado
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+//Nova variável "cep" somente com dígitos
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado
+if (cep != "") {
+
+    //Expressão regular para validar o CEP
+    var validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if(validacep.test(cep)) {
+
+        document.getElementById('cep').value = cep.substring(0,5) +"-" + cep.substring(5);
+
+        //Preenche os campos com "..." enquanto consulta webservice
+        document.getElementById('endereco').value="...";
+        document.getElementById('bairro').value="...";
+        document.getElementById('cidade').value="...";
+        document.getElementById('uf').value="...";
+
+        //Cria um elemento javascript
+        var script = document.createElement('script');
+
+        //Sincroniza com o callback
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+        //Insere script no documento e carrega o conteúdo
+        document.body.appendChild(script);
+
+    } 
+    else {
+        //cep é inválido
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+} 
+else {
+    //cep sem valor, limpa formulário
+    limpa_formulário_cep();
+}
+};
+function validar(){
+    alert('Cadastro realizado!')
+}
